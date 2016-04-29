24cf4159bff16405d1decbcb2bbaba43ccf938d8
Kai Ovesen <ovesenk@onid.oregonstate.edu>
2016-04-28 18:25:13 -0700

    Reformatted the unit test to clean it up and fixed a typo in an assert.

--- dominion/unittest3.c
+++ dominion/unittest3.c
@@ -7,18 +7,8 @@
 int main(int arc, char *argv[]) {
   printf("Running unit test for function \"intializeGame()\"...\n");
 
-  struct gameState G1;
-  struct gameState *p1 = &G1;
-  struct gameState G2;
-  struct gameState *p2 = &G2;
-  struct gameState G3;
-  struct gameState *p3 = &G3;
-  struct gameState G4;
-  struct gameState *p4 = &G4;
-  struct gameState G5;
-  struct gameState *p5 = &G5;
-  struct gameState Gbad;
-  struct gameState *pbad = &Gbad;
+  struct gameState G;
+  struct gameState *p = &G;
 
   int seed = 123456789;
   int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
@@ -26,27 +16,26 @@ int main(int arc, char *argv[]) {
   int kBad[10] = {adventurer, adventurer, embargo, village, minion, mine, cutpurse, 
 	       sea_hag, tribute, smithy};
 
-  assertTrue(initializeGame(1, k, seed, p1) == -1, "initializeGame(1, k, seed, p1) == -1");
-  assertTrue(initializeGame(3, k, seed, p3) == 0, "initializeGame(3, k, seed, p3) == 0");
-  assertTrue(initializeGame(4, k, seed, p4) == 0, "initializeGame(4, k, seed, p4) == 0");
-  assertTrue(initializeGame(5, k, seed, p5) == -1, "initializeGame(5, k, seed, p5) == -1");
-  assertTrue(initializeGame(2, kBad, seed, pbad) == -1, "initializeGame(2, kBad, seed, pbad) == -1");
-  
-  //initializeGame(2, k, seed, p);
-  assertTrue(initializeGame(2, k, seed, p2) == 0, "initializeGame(2, k, seed, p2) == 0");
-  assertTrue(p2->numPlayers == 2, "p2->numPlayers == 2");
-  assertTrue(p2->supplyCount[curse] == 10, "p2->supplyCount[curse] == 10");
-  assertTrue(p2->supplyCount[estate] == 8, "p2->supplyCount[estate] == 8");
-  assertTrue(p2->supplyCount[duchy] == 8, "p2->supplyCount[duchy] == 8");
-  assertTrue(p2->supplyCount[province] == 8, "p2->supplyCount[province] == 8");
-  assertTrue(p2->supplyCount[copper] == 46, "p2->supplyCount[copper] == 46");
-  assertTrue(p2->supplyCount[silver] == 40, "p2->supplyCount[silver] == 40");
-  assertTrue(p2->supplyCount[gold] == 30, "p2->supplyCount[gold] == 30");
-  assertTrue(p2->embargoTokens[0] == 0, "p2->embargoTokens[0] == 0)");
+  assertTrue(initializeGame(2, k, seed, p) == 0, "initializeGame(2, k, seed, p) == 0");
+  assertTrue(p->numPlayers == 2, "p->numPlayers == 2");
+  assertTrue(p->supplyCount[curse] == 10, "p->supplyCount[curse] == 10");
+  assertTrue(p->supplyCount[estate] == 8, "p->supplyCount[estate] == 8");
+  assertTrue(p->supplyCount[duchy] == 8, "p->supplyCount[duchy] == 8");
+  assertTrue(p->supplyCount[province] == 8, "p->supplyCount[province] == 8");
+  assertTrue(p->supplyCount[copper] == 46, "p->supplyCount[copper] == 46");
+  assertTrue(p->supplyCount[silver] == 40, "p->supplyCount[silver] == 40");
+  assertTrue(p->supplyCount[gold] == 30, "p->supplyCount[gold] == 30");
+  assertTrue(p->embargoTokens[0] == 0, "p->embargoTokens[0] == 0)");
+  assertTrue(p->deckCount[0] == 5, "p->deckCount[0] == 5"); //10 cards originally but drew 5 cards
+  assertTrue(p->deckCount[1] == 10, "p->deckCount[1] == 10"); 
+  assertTrue(p->deckCount[2] == 0, "p->deckCount[2] == 0");
 
-  assertTrue(p2->deckCount[0] == 5, "p2->deckCount[0] == 5"); //10 cards originally but drew 5 cards
-  assertTrue(p2->deckCount[1] == 5, "p2->deckCount[1] == 10"); 
-  assertTrue(p2->deckCount[2] == 0, "p2->deckCount[2] == 0");
+  assertTrue(initializeGame(1, k, seed, p) == -1, "initializeGame(1, k, seed, p) == -1");
+  assertTrue(initializeGame(3, k, seed, p) == 0, "initializeGame(3, k, seed, p) == 0");
+  assertTrue(initializeGame(4, k, seed, p) == 0, "initializeGame(4, k, seed, p) == 0");
+  assertTrue(initializeGame(5, k, seed, p) == -1, "initializeGame(5, k, seed, p) == -1");
+  assertTrue(initializeGame(2, kBad, seed, p) == -1, "initializeGame(2, kBad, seed, p) == -1");
+  
 
   return 0;
 }
