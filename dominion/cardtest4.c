51ce2075f1bf13be3aa254418a89fe3a5891dded
Kai Ovesen <ovesenk@onid.oregonstate.edu>
2016-04-28 18:35:39 -0700

    Removed incorrect bug comment.

--- dominion/cardtest4.c
+++ dominion/cardtest4.c
@@ -23,7 +23,7 @@ int main() {
   p->hand[0][0] = village;
   assertTrue(playCard(0, 0, 0, 0, p) == 0, "playCard(0, 0, 0, 0, p) == 0");
   assertTrue(numHandCards(p) == numHandCardsBefore, "numHandCards(p) == numHandCardsBefore");
-  assertTrue(p->numActions == numActionsBefore + 2 - 1, "p->numActions == numActionsBefore + 2 - 1"); //BUG: Actions should increase by 2, not 1
+  assertTrue(p->numActions == numActionsBefore + 2 - 1, "p->numActions == numActionsBefore + 2 - 1"); // Add 2, sub 1 from playCard()
 
   return 0;
 }
